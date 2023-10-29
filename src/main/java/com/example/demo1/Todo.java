14a2d4381feeaf46142818acd476d5580c1521b9
wajahat ahmed <wajahatahmed@192.168.1.9>
2023-10-29 17:50:36 +0500

    created the object

--- src/main/java/com/example/demo1/Todo.java
+++ src/main/java/com/example/demo1/Todo.java
@@ -1,4 +1,72 @@
 package com.example.demo1;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    private String task;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+    private LocalDate dateCompleted;
+    private LocalDate dateCreated;
+
+    @PrePersist
+    private void init() {
+        setDateCreated(LocalDate.now());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public void setDateCompleted(LocalDate dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public LocalDate getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
 }
